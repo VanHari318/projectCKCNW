115f8748753df8b9fe7530df0a22f7126e301528
VanHari318 <2351060441@e.tlu.edu.vn>
2026-06-24 18:23:39 +0700

    fix: trust all proxies to enforce HTTPS on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'checkrole' => \App\Http\Middleware\CheckRole::class,
             'setlocale' => \App\Http\Middleware\SetLocale::class,
